58692d784133b9d1215eef37f8540ccd7678194a
Pablo Reyes <pablo.reyes.moctezuma@gmail.com>
2021-04-19 22:37:23 -0500

    Create output dir before downloading

--- Bin/get_data.bash
+++ Bin/get_data.bash
@@ -1,3 +1,6 @@
+# Create output dir
+mkdir -p Data
+
 # Download using wget
 # -q --show-progress forces wget to only show the progress bar
 wget -q --show-progress $DATA_URL -O Data/marco_integrado.zip
